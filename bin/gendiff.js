77e41c3ea61291d1a00dfa5fc2a2160b6cb0fcdb
tigp <hnztkfc@gmail.com>
2022-03-29 19:53:11 +0300

    fixed gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { genDiff } from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +9,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1', '-v')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
-
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();

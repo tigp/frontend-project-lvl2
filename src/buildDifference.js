ca32867dedbe2d26173e592aa84fdb1cc3f417e1
tigp <hnztkfc@gmail.com>
2022-03-29 19:56:45 +0300

    maked a new folder

---
+++ src/buildDifference.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import _ from 'lodash';
+
+const buildDifference = (file1, file2) => {
+  const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+
+  const diff = keys.map((key) => {
+    if (_.has(file1, key) && !_.has(file2, key)) {
+      return `- ${key}: ${file1[key]}\n`;
+    }
+    if (!_.has(file1, key) && _.has(file2, key)) {
+      return `+ ${key}: ${file2[key]}\n`;
+    }
+    if ((_.has(file1, key) && _.has(file2, key) && file1[key] === file2[key])) {
+      return `  ${key}: ${file2[key]}\n`;
+    }
+    if ((_.has(file1, key) && _.has(file2, key) && file1[key] !== file2[key])) {
+      return [`- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}\n`];
+    }
+    return `{\n${diff.join('')}}`;
+  });
+  return diff;
+};
+
+export default buildDifference;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import path from 'path';
+import fs from 'fs';
+import { buildDifference } from './buildDifference.js';
+
+const getPath = (filename) => path.resolve(process.cwd(), filename);
+
+const readFile = (filePath) => {
+  const file = filePath.startsWith(getPath) ? fs.readFileSync(filePath, 'utf8') : fs.readFileSync(`${getPath}/${filePath}`, 'utf8');
+  if (file.endsWith('.json')) {
+    return JSON.parse(file);
+  }
+
+  return 'incorrect file';
+};
+
+const genDiff = (filePath1, filePath2) => {
+  const file1 = readFile(filePath1);
+  const file2 = readFile(filePath2);
+  console.log(buildDifference(file1, file2));
+
+  return buildDifference(file1, file2);
+};
+
+export default genDiff;

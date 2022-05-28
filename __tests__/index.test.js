047cc7b58de5eb04a039a9f8ff033dfafb9224e0
tigp <hnztkfc@gmail.com>
2022-05-28 20:01:26 +0300

    created tests for formatters and added coverage test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,20 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gediff', () => {
-  const expected = fs.readFileSync(getFixturePath('expected'), 'utf-8');
-  const test1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const test2 = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  const test3 = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.json'));
+describe.each(['stylish', 'plain', 'json'])("gendiff's format %s", (format) => {
+  const expected = readFile(`expected_${format}.txt`);
 
-  expect(test1).toEqual(expected);
-  expect(test2).toEqual(expected);
-  expect(test3).toEqual(expected);
+  test.each(['json', 'yml'])("file's format %s", (fileExtension) => {
+    const filename1 = getFixturePath(`file1.${fileExtension}`);
+    const filename2 = getFixturePath(`file2.${fileExtension}`);
+
+    expect(genDiff(filename1, filename2, format)).toEqual(expected);
+  });
 });
